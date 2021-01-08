69129e65119cd9c5ca1e04a5e7818c8b7c1c985e
kirkhellsten <60529153+kirkhellsten@users.noreply.github.com>
2021-01-08 16:26:48 -0700

    Naming convention update
    Updated file naming convention to be closer to Hungarian Notation. Removed prepended abbreviation "p" from parameter variables for all functions.

--- GeneralMathematics.h
+++ GeneralMathematics.h
@@ -1,57 +1,57 @@
 #ifndef GENERALMATHEMATICS_H_INCLUDED
 #define GENERALMATHEMATICS_H_INCLUDED
 
-unsigned int gm_factorial_recursion_head(unsigned int pN);
+unsigned int fnGMfactorialRecursionHead(unsigned int n);
 
-// Factorial coded using for loop, finding the series of products from pN to 1
+// Factorial coded using for loop, finding the series of products from n to 1
 // Do not need to compute some number N times 1 since equals N, thus condition i > 1 used,
-// in the case pN = 0 ret variable initially set to 1, this handles 0! = 1
-unsigned int gm_factorial_for_iteration(unsigned int pN) {
-    unsigned int ret = 1;
-    for (unsigned int i = pN; i > 1; --i) {
-        ret *= i;
+// in the case n = 0 factorial initially set to 1, this handles 0! = 1
+unsigned int fnGMFactorialForIteration(unsigned int n) {
+    unsigned int factorial = 1;
+    for (unsigned int i = n; i > 1; --i) {
+        factorial *= i;
     }
-    return ret;
+    return factorial;
 }
 
 // Factorial coded using do loop
-// Same loop condition applies as seen in for loop iteration except on pN variable
-unsigned int gm_factorial_do_iteration(unsigned int pN) {
-    unsigned int ret = 1;
+// Same loop condition applies as seen in for loop iteration except on n
+unsigned int fnGMFactorialDoIteration(unsigned int n) {
+    unsigned int factorial = 1;
     do {
-        ret *= pN;
-        --pN;
-    } while (pN > 1);
-    return ret;
+        factorial *= n;
+        --n;
+    } while (n > 1);
+    return factorial;
 }
 
 // Factorial coded using while loop
-// Same loop condition applies as seen in for loop iteration except on pN variable
-unsigned int gm_factorial_while_iteration(unsigned int pN) {
-    unsigned int ret = 1;
-    while (pN > 1) {
-        ret *= pN;
-        --pN;
+// Same loop condition applies as seen in for loop iteration except on n
+unsigned int fnGMFactorialWhileIteration(unsigned int n) {
+    unsigned int factorial = 1;
+    while (n > 1) {
+        factorial *= n;
+        --n;
     };
-    return ret;
+    return factorial;
 }
 
 // Factorial coded using head recursion
-unsigned int gm_factorial_recursion_head(unsigned int pN) {
-    if (pN == 1) {
-        return pN;
+unsigned int fnGMFactorialRecursionHead(unsigned int n) {
+    if (n == 1) {
+        return n;
     } else {
-        return pN * gm_factorial_recursion_head(pN - 1);
+        return n * fnGMFactorialRecursionHead(n - 1);
     }
 }
 
 // Factorial coded using tail recursion
-// pRunningTotal set to default value 1, this handles in case pN <= 2
-unsigned int gm_factorial_recursion_tail(unsigned int pN, unsigned int pRunningTotal = 1) {
-    if (pN <= 1) {
-        return pRunningTotal;
+// runningTotal set to default value 1, this handles in case n <= 1
+unsigned int fnGMFactorialRecursionTail(unsigned int n, unsigned int runningTotal = 1) {
+    if (n <= 1) {
+        return runningTotal;
     } else {
-        return gm_factorial_recursion_tail(pN - 1, pN * pRunningTotal);
+        return fnGMFactorialRecursionTail(n - 1, n * runningTotal);
     }
 }
 
