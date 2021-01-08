e497b091825f2fed5a6e9c1958f89dd007bf115a
kirkhellsten <60529153+kirkhellsten@users.noreply.github.com>
2021-01-08 16:28:26 -0700

    Function name change
    GeneralMathematics.h file naming convention updated to be closer to Hungarian Notation. This required modification to function call in main.cpp

--- main.cpp
+++ main.cpp
@@ -8,19 +8,19 @@ int main()
 {
 
     cout << "Factorial of 10 using For Iteration: " << endl;
-    cout << gm_factorial_for_iteration(10) << endl;
+    cout << fnGMFactorialForIteration(10) << endl;
 
     cout << "Factorial of 10 using Do Iteration: " << endl;
-    cout << gm_factorial_do_iteration(10) << endl;
+    cout << fnGMFactorialDoIteration(10) << endl;
 
     cout << "Factorial of 10 using While Iteration: " << endl;
-    cout << gm_factorial_while_iteration(10) << endl;
+    cout << fnGMFactorialWhileIteration(10) << endl;
 
     cout << "Factorial of 10 using Head Recursion: " << endl;
-    cout << gm_factorial_recursion_head(10) << endl;
+    cout << fnGMFactorialRecursionHead(10) << endl;
 
     cout << "Factorial of 10 using Tail Recursion: " << endl;
-    cout << gm_factorial_recursion_tail(10) << endl;
+    cout << fnGMFactorialRecursionTail(10) << endl;
 
     return 0;
 }
